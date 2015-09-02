c871e92c076b1e89b9f277ec8dadf68d71616f9c
dajacobs <dajacobs.tab@gmail.com>
2015-09-02 11:06:27 -0500

    forkblur

---
+++ dajacobs/src/ForkBlur.java
@@ -0,0 +1,61 @@
+
+import java.util.concurrent.RecursiveAction;
+
+public class ForkBlur extends RecursiveAction {
+    private final int[] mSource;
+    private final int mStart;
+    private final int mLength;
+    private final int[] mDestination;
+  
+    // Processing window size; should be odd.
+    private final int mBlurWidth = 15;
+  
+    public ForkBlur(int[] src, int start, int length, int[] dst) {
+        mSource = src;
+        mStart = start;
+        mLength = length;
+        mDestination = dst;
+    }
+
+    protected void computeDirectly() {
+        int sidePixels = (mBlurWidth - 1) / 2;
+        for (int index = mStart; index < mStart + mLength; index++) {
+            // Calculate average.
+            float rt = 0, gt = 0, bt = 0;
+            for (int mi = -sidePixels; mi <= sidePixels; mi++) {
+                int mindex = Math.min(Math.max(mi + index, 0),
+                                    mSource.length - 1);
+                int pixel = mSource[mindex];
+                rt += (float)((pixel & 0x00ff0000) >> 16)
+                      / mBlurWidth;
+                gt += (float)((pixel & 0x0000ff00) >>  8)
+                      / mBlurWidth;
+                bt += (float)((pixel & 0x000000ff) >>  0)
+                      / mBlurWidth;
+            }
+          
+            // Reassemble destination pixel.
+            int dpixel = (0xff000000     ) |
+                   (((int)rt) << 16) |
+                   (((int)gt) <<  8) |
+                   (((int)bt));
+            mDestination[index] = dpixel;
+        }
+    }
+    protected static int sThreshold = 100000;
+
+    @Override
+    protected void compute() {
+        if (mLength > sThreshold) {
+        } else {
+            computeDirectly();
+            return;
+        }
+
+        int split = mLength / 2;
+
+        invokeAll(new ForkBlur(mSource, mStart, split, mDestination),
+                  new ForkBlur(mSource, mStart + split, mLength - split,
+                               mDestination));
+        }
+}
