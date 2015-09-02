f78a1c565c7de478d5ab1fa78cd80e89f8207915
dajacobs <dajacobs.tab@gmail.com>
2015-09-02 09:24:26 -0500

    simple counter

---
+++ dajacobs/src/app/Counter.java
@@ -0,0 +1,14 @@
+package app;
+
+class Counter {
+    private int c = 0;
+    public void increment() {
+        c++;
+    }
+    public void decrement() {
+        c--;
+    }
+    public int value() {
+        return c;
+    }
+}
