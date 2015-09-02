db3cd9f1d34cb64de55850d4f9b382a2ab774738
dajacobs <dajacobs.tab@gmail.com>
2015-09-02 09:59:36 -0500

    producer

---
+++ dajacobs/src/app/Producer.java
@@ -0,0 +1,26 @@
+package app;
+
+import java.util.Random;
+
+public class Producer implements Runnable {
+    private Drop drop;
+    public Producer(Drop drop) {
+        this.drop = drop;
+    }
+    public void run() {
+        String importantInfo[] = {
+            "Mares eat oats",
+            "Does eat oats",
+            "Little lambs eat ivy",
+            "A kid will eat ivy too"
+        };
+        Random random = new Random();
+        for (String importantInfo1 : importantInfo) {
+            drop.put(importantInfo1);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {}
+        }
+        drop.put("DONE");
+    }
+}
