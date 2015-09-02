963ce0c6edcedd38ba69e1cd6e3179a319959278
dajacobs <dajacobs.tab@gmail.com>
2015-09-02 08:31:02 -0500

    simple runnable

---
+++ dajacobs_HW1/src/dajacobs_hw1/HelloRunnable.java
@@ -0,0 +1,11 @@
+package dajacobs_hw1;
+
+public class HelloRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+    public static void main(String[] args) {
+        (new Thread(new HelloRunnable())).start();
+    }    
+}
