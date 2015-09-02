c5e21c7b58409a6aa68869548df5fc3b1218d24b
dajacobs <dajacobs.tab@gmail.com>
2015-09-02 08:32:32 -0500

    simple sleep messages

---
+++ dajacobs_HW1/src/dajacobs_hw1/SleepMessages.java
@@ -0,0 +1,29 @@
+package dajacobs_hw1;
+
+public class SleepMessages {
+    public static void main(String args[]) throws InterruptedException {
+        String importantInfo[] = {
+            "Mares eats oats",
+            "Does eat oats",
+            "Little lambs eat ivy",
+            "A kid will eat ivy too"
+        };
+        for (String importantInfo1 : importantInfo) {
+            // Pause for 4 seconds
+            Thread.sleep(4000);
+            // Print a message
+            System.out.println(importantInfo1);
+        }
+        for (String importantInfo1 : importantInfo) {
+            // Pause for 4 seconds
+            try {
+                Thread.sleep(4000);
+            } catch (InterruptedException e) {
+                // We've been interrupted: no more messages.
+                return;
+            }
+            // Print a message
+            System.out.println(importantInfo1);
+        }
+    }
+}
